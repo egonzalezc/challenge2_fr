28bfc8074d6bbcf795691dcb279a6dc705debe4b
Esther Gonzalez <egonzalez@mahisoft.com>
2017-05-12 13:15:45 +0200

    Fix eigenvector calculation and some minor changes

--- pca_facerecognition.m
+++ pca_facerecognition.m
@@ -1,120 +1,110 @@
 
 global sizeImg;
 sizeImg = [70,160];
-neigen_faces = 10; %Quantity of eigenvectors to keep
-
-%read training and tests data
-training_faces = readData('training/');
-test_faces = readData('test/');
+numEigenvectors = 10;
 
 % call training
-[eigen_vectors, meanface] = training(training_faces, neigen_faces);
+trainSet = readData('training/');
+[eigenvectors, meanFace] = training(trainSet, numEigenvectors);
 
 % call test
+testSet = readData('test/');
 threshold = 20;
-for i=1:size(test_faces,2)
-    testing(test_faces(:,i), eigen_vectors, meanface, threshold); 
+for i=1:size(testSet,2)
+    testing(test_faces(:,i), eigenvectors, meanFace, threshold); 
 end
 
 %%
 %PCA
-function [eigen_vectors, meanfaces] = training(faces, neigen_faces)    
-    %Calculate the mean
-    meanfaces=mean(faces,2); 
-    normalized_faces = faces - meanfaces;
-
-%     displayImage(meanfaces);
+function [eigenvectors, meanFace] = training(imagesMatrix, numEigenvectors)    
+    meanFace=mean(imagesMatrix,2); 
+    normImages = imagesMatrix - meanFace;
+    figure;
+    imshow(getRGBImage(meanFace));
     
-    %Calculate the covariance matrix.
-%     covfaces=faces*faces';
-    L=normalized_faces'*normalized_faces;
-
-    %Calculate the eigenvalues and eigenvectors.
-%     [eigen_vectors,eigen_values]=eig(covfaces);
-     eigen_vectors= normalized_faces*L;
-     eigen_vectors = normc(eigen_vectors);
+    %Compute eigenvectors and eigenvalues using a smaller matrix than the conv
+    smallMatrix=normImages'*normImages;
+    [eigenvectors,eigenvalues]=eig(smallMatrix);
+    eigenvectors= normImages*eigenvectors;
+    eigenvectors = normc(eigenvectors);
     
-     %sort eigen values in descending order so the first K eigenvectors will
+    %sort eigen values in descending order so the first K eigenvectors will
     %be those with the most relevant information
-%     [value, column] = sort(diag(eigen_values), 'descend');
-%     eigen_vectors = eigen_vectors(:,column);
+    [value, column] = sort(diag(eigenvalues), 'descend');
+    eigenvectors = eigenvectors(:,column);
 
-    displayEigenFaces(eigen_vectors, neigen_faces);
+    displayEigenFaces(eigenvectors, numEigenvectors);
  
     %Select the first 10 eigenvectors
-    eigen_vectors = eigen_vectors(:,1:neigen_faces); 
-
-    
+    eigenvectors = eigenvectors(:,1:numEigenvectors); 
+  
 end
 
 %% Test eigenmouth/ Detect or no a mouth in the given input vector
-function [] = testing(input_vector, eigen_vectors, meanface, threshold)
+function [] = testing(inputImgVector, eigenvectors, meanFace, threshold)
     global sizeImg;
-    normalized_input = input_vector - meanface;
+    normInput = inputImgVector - meanFace;
+    
     %project on the eigenspace
-    weights = (eigen_vectors' * normalized_input);
-    projection =  eigen_vectors * weights;
+    weights = (eigenvectors' * normInput);
+    projection =  eigenvectors * weights;
     
-    img = displayImage(projection);
-    
-    distance = norm(normalized_input - projection);
-    distance_score = 1/(1+distance);
+    distance = norm(normInput - projection);
+    distanceScore = 1/(1+distance);
 %     if distance < threshold
-%             fprintf( 'It is a mouth with a distance of %f score %f', distance, distance_score);
+%             fprintf( 'It is a mouth with a distance of %f score %f', distance, distanceScore);
 %         else
 %             fprintf( 'It is not a mouth');
 %     end  
+    img = getRGBImage(projection);
     imwrite(uint8(img), sprintf('results/Projection_%f.jpg',distance));
-    imwrite(reshape(input_vector, [sizeImg,3]), sprintf('results/Original_%f.jpg',distance));
+    imwrite(reshape(inputImgVector, [sizeImg,3]), sprintf('results/Distance=%f.jpg',distance));
 end
 
 %% Tools
-function [faces] =  readData(path)
+function [mouths] =  readData(path)
     global sizeImg;
     global nimages;
     
-    folder_name = strcat(path,'*.jpg');
-    imagefiles = dir(folder_name);
+    folderName = strcat(path,'*.jpg');
+    imagefiles = dir(folderName);
     nimages = length(imagefiles);
-    faces = zeros(prod([sizeImg,3]), nimages);
+    mouths = zeros(prod([sizeImg,3]), nimages);
     for i=1:nimages
        filename = strcat(path, imagefiles(i).name);
-       currentimage = im2double(imread(filename));
-       currentimage = imresize(currentimage,sizeImg);
-       vectorimage = reshape(currentimage, 1, []);
-       faces(:, i) = vectorimage; %Matrix of images vector
+       currentImage = im2double(imread(filename));
+       currentImage = imresize(currentImage,sizeImg);
+       vectorImage = reshape(currentImage, 1, []);
+       mouths(:, i) = vectorImage;
     end
 end
 
-function displayEigenFaces(eigen_vectors, neigen_faces)
+function displayEigenFaces(eigenVectors, numEigenvectors)
     global sizeImg;
     rows =  prod([sizeImg,3]);
 
-     minvalue = min(eigen_vectors);
-     eigen_vectors = eigen_vectors-repmat(minvalue, [rows, 1]);
-     maxvalue = max(eigen_vectors);
+     minvalue = min(eigenVectors);
+     eigenVectors = eigenVectors-repmat(minvalue, [rows, 1]);
+     maxvalue = max(eigenVectors);
 
-     eigen_vectors = 255* eigen_vectors./repmat(maxvalue, [rows, 1]);
+     eigenVectors = 255* eigenVectors./repmat(maxvalue, [rows, 1]);
     
     figure;
     colormap hsv;
-    for i = 1:neigen_faces
+    for i = 1:numEigenvectors
         subplot(5,2,i);
-        v = reshape(eigen_vectors(:,i),[sizeImg,3]);
+        v = reshape(eigenVectors(:,i),[sizeImg,3]);
         imshow(uint8(v));
         title(sprintf('Eigenface %d',i));
     end
 end
 
-function [img] = displayImage(vector_image)
-    global sizeImg;
-    minvalue = min(vector_image);
-    vector_image = vector_image-minvalue;
-    maxvalue = max(vector_image);
-    vector_image = 255* vector_image./maxvalue;
 
-%     figure;
-    colormap hsv;
-    img = reshape(vector_image,[sizeImg,3]);
-%     imshow(uint8(img));   
+function [imgRGB] = getRGBImage(vectorImage)
+    global sizeImg;
+    minvalue = min(vectorImage);
+    vectorImage = vectorImage-minvalue;
+    maxvalue = max(vectorImage);
+    vectorImage = 255* vectorImage./maxvalue;
+    imgRGB = uint8(reshape(vectorImage,[sizeImg,3]));
 end
